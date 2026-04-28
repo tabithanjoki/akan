eaaa10beda680a4081204d26a7feca3da809dd7a
Tabitha Njoki <t8080438@gmail.com>
2026-04-28 11:51:15 +0300

    akan

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // assigning the akan names to the two genders
 const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-if( gender=)
+
+let akanName
+if( gender==="male" {
+    akanName = maleNames}){}
+
+else()
 
 
 
